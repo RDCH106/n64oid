729d907afbe8175969f61a5364c3ac4325373fc4
scottgl <scottgl@c861efd2-eb65-11de-a198-37193048d1ed>
2010-01-11 19:21:40 +0000

    The SDL original include files from SDL 1.2.13.

--- SDL/src/video/windib/SDL_gapidibvideo.h
+++ SDL/src/video/windib/SDL_gapidibvideo.h
@@ -19,4 +19,38 @@
     Sam Lantinga
     slouken@libsdl.org
 */
+#include "SDL_config.h"
 
+#ifndef _SDL_gapidibvideo_h
+#define _SDL_gapidibvideo_h
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+/* Hidden "this" pointer for the video functions */
+#define _THIS	SDL_VideoDevice *this
+
+/* typedef these to be able to define pointers, but still force everybody who
+ * wants to access them to include the corresponding header */
+typedef struct GapiInfo GapiInfo;
+typedef struct DibInfo DibInfo;
+
+/* for PDA */
+typedef enum
+{
+	SDL_ORIENTATION_UP,
+	SDL_ORIENTATION_DOWN,
+	SDL_ORIENTATION_LEFT,
+	SDL_ORIENTATION_RIGHT
+} SDL_ScreenOrientation;
+
+/* Private display data shared by gapi and windib*/
+struct SDL_PrivateVideoData {
+	int supportRotation; /* for Pocket PC devices */
+	DWORD origRotation; /* for Pocket PC devices */
+	
+	GapiInfo* gapiInfo;
+	DibInfo* dibInfo;
+};
+
+#endif
